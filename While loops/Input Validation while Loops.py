f344dc6561600c05f184d291318d30fbaee53d9c
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-17 12:12:34 -0600

    all done

---
+++ While loops/Input Validation while Loops.py
@@ -0,0 +1,4 @@
+x, y = 0, 1
+while x < 10:
+    print(x)
+    x, y = y, x+y
---
+++ While loops/input_while.py
@@ -0,0 +1,9 @@
+# list to store guesses
+empty_list = []
+# user input
+guessed_number = int(input('Enter a number between 1 and 100.'))
+while 1 <= guessed_number >= 100:
+    int(input('Enter a number between 1 and 100.'))
+    empty_list.append(guessed_number)
+    print(empty_list)
+
