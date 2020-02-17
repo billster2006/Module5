fd995458d2707548e58c33cbaa9199b4ecfcb332
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-17 12:23:31 -0600

    all done

---
+++ Use break, else, and continue in Loops/Test.py
@@ -0,0 +1,5 @@
+veggies = ['artichoke', 'cucumber', 'green beans']
+for v in veggies:
+    print(v)
+    if v == 'cucumber':
+        break
---
+++ Use break, else, and continue in Loops/input_while_exit.py
@@ -0,0 +1,10 @@
+# list to store guesses
+empty_list = []
+# user input
+guessed_number = int(input('Enter a number between 1 and 100.'))
+while 1 <= guessed_number <= 100:
+    empty_list.append(guessed_number)
+    print(empty_list)
+    guessed_number = int(input('Enter a number between 1 and 100.'))
+    guessed_number = 1 >= guessed_number <= 100
+    break
